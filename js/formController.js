741102a384acda1bcd7e06bf6e17da8ea2d2ba31
nomad775 <foley10x@yahoo.com>
2022-04-06 15:47:30 -0500

    form scripts to new files

---
+++ js/formController.js
@@ -0,0 +1,181 @@
+let originName = "Kerbin";
+let destinationName = "Duna";
+let disabledDestName = "Kerbin";
+
+function originChange() {
+
+    console.log("changing origin");
+
+    originName = document.forms["origin-destination"]["origin"].value;
+
+    toggleDestinationOption(originName);
+
+    destinationName = document.forms["origin-destination"]["destination"].value
+
+    txOrbit.originPlanet = planets[originName];
+    txOrbit.destinationPlanet = planets[destinationName];
+
+    updateQueryString();
+
+    displayedTime = txOrbit.solveTForRdv(currentTime);
+
+    window.dispatchEvent(displayedTimeChangeEvent);
+
+    zoomTxOrbit();
+
+}
+
+function toggleDestinationOption(originName) {
+
+    // enable destination option that was disabled before origin change
+    let disabledDest = document.forms["origin-destination"]["optDest" + disabledDestName];
+    disabledDest.disabled = false;
+
+    console.log("destination " + disabledDestName + " re-enabled");
+
+    console.log("origin changed to " + originName);
+
+    // disable destination option that is the same as the current origin
+    // so that origin and destinationcannot be the same
+    disabledDestName = originName;
+    disabledDest = document.forms["origin-destination"]["optDest" + disabledDestName];
+    disabledDest.disabled = true;
+
+    console.log("destination " + disabledDestName + " disabled");
+
+    // if new origin is the current destination then
+    // set new destination to Kerbin or
+    // if new origin is Kerbin, set new destination to Duna
+
+    if (disabledDest.checked) {
+
+        let kerbin = document.forms["origin-destination"]["optDestKerbin"];
+        let duna = document.forms["origin-destination"]["optDestDuna"]
+
+        //set destination to kerbin
+        //destinationOption.checked=false;
+        //kerbin.checked=true;
+
+        if (originName == "Kerbin") {
+            //duna.disabled = false;
+            //duna.checked = true;
+            duna.click();
+        } else {
+
+            console.log("origin was destination.");
+            console.log("changing destination to kerbin");
+            kerbin.click();
+        }
+    }
+}
+
+function destinationChange() {
+
+    destinationName = document.forms["origin-destination"]["destination"].value;
+    txOrbit.destinationPlanet = planets[destinationName];
+
+    displayedTime = txOrbit.solveTForRdv(currentTime);
+    window.dispatchEvent(displayedTimeChangeEvent);
+
+    updateQueryString();
+    zoomTxOrbit();
+
+    console.log("destination changed to " + destinationName);
+}
+
+function currentTimeChange() {
+
+    console.log('current time changed')
+
+    let y = document.forms["origin-destination"]["utY"].value;
+    let d = document.forms["origin-destination"]["utD"].value;
+    let h = document.forms["origin-destination"]["utH"].value;
+    let m = document.forms["origin-destination"]["utM"].value;
+
+    currentTime = (y - 1) * secondsPerYear + (d - 1) * secondsPerDay + h * secondsPerHour + m * secondsPerMinute;
+
+    displayedTime = txOrbit.solveTForRdv(currentTime);
+    window.dispatchEvent(displayedTimeChangeEvent);
+
+    document.forms["origin-destination"]["utSeconds"].value = currentTime;
+    document.getElementById("outCurrentTime").textContent = convertSecondsToDateObj(currentTime).toString();
+
+    updateQueryString();
+}
+
+function displayedTimeChange(event) {
+
+    //console.log("time change caught : display time " + displayedTime);
+
+    document.getElementById("outDisplayedTime").textContent = convertSecondsToDateObj(displayedTime).toString();
+    document.forms["origin-destination"]["tod"].value = Math.round(displayedTime);
+    document.forms["origin-destination"]["toa"].value = Math.round(txOrbit.toa);
+
+    updateOutput();
+}
+
+function updateOutput() {
+
+    document.getElementById("outOrigin").textContent = txOrbit.originPlanet.name;
+    document.getElementById("outDestination").textContent = txOrbit.destinationPlanet.name;
+    document.getElementById("outEtd").textContent = convertSecondsToDateObj(displayedTime).toString();
+    document.getElementById("outOriginLnEtd").textContent = radToDeg(txOrbit.Ln_o);
+    document.getElementById("outDestinationLnEtd").textContent = radToDeg(txOrbit.Ln_d);
+    document.getElementById("outPhaeAngleEtd").textContent = radToDeg(txOrbit.phaseAngle);
+
+    document.getElementById("outEta").textContent = convertSecondsToDateObj(txOrbit.toa).toString();
+    document.getElementById("outOriginLnEta").textContent = radToDeg(txOrbit.Ln_o);
+    document.getElementById("outDestinationLnEta").textContent = radToDeg(txOrbit.Ln_da);
+
+    document.getElementById("outTxA").textContent = txOrbit.a.toFixed(0);
+    document.getElementById("outTxE").textContent = txOrbit.e.toFixed(4);
+    document.getElementById("outTxI").textContent = 0;
+    document.getElementById("outTxArgPe").textContent = radToDeg(txOrbit.Ln_pe);
+    document.getElementById("outTxLan").textContent = 0;
+
+}
+
+function updateQueryString() {
+
+    let frmElement = document.querySelector("form");
+    let frmData = new FormData(frmElement);
+
+    let params = new URLSearchParams(frmData);
+
+    let url = window.location.pathname;
+    url += "?" + params.toString();
+
+    //const url = new URL(window.location.href);
+    //url.searchParams.set(key, value);
+    window.history.pushState({}, '', url.toString());
+}
+
+function onSubmit(event) {
+
+    // if form action attribute is blank then form data is appended to current location
+    // inject location with parameter into history to back button returns to current state
+
+    let frmElement = document.querySelector("form");
+    let frmData = new FormData(frmElement);
+
+    let params = new URLSearchParams(frmData);
+
+    let url = window.location.pathname;
+    url += "?" + params.toString();
+
+    console.log(params.toString());
+    console.log(url);
+
+    window.history.pushState({}, "", url);
+
+
+    //event.preventDefault();
+
+    // for (let [name, value] of fd) {
+    //         params.append(`${name}`, `${value}`); // key1 = value1, then key2 = value2
+    // }
+
+
+    return;
+
+}
---
+++ js/main_form.js
@@ -0,0 +1,83 @@
+
+const planets = { };
+
+var txOrbit;
+
+var currentTime = 0;
+var displayedTime = 0;
+
+function initializeFromQueryString(){
+
+    let params = new URLSearchParams(location.search);
+    let hasParams = params.has("utY");
+    
+    if(!hasParams){
+        console.log("no history");
+        return false;
+    }
+
+    console.log("from history");
+    console.log(params.get("origin"));
+    console.log(params.get("destination"));
+
+    originName = params.get("origin");
+    destinationName = params.get("destination");
+    disabledDestName = originName;
+
+    let y = params.get("utY");
+    let d = params.get("utD");
+    let h = params.get("utH");
+    let m = params.get("utM");
+
+    currentTime = (y - 1) * secondsPerYear + (d - 1) * secondsPerDay + h * secondsPerHour + m * secondsPerMinute;
+
+    document.forms["origin-destination"]["utY"].value = y;
+    document.forms["origin-destination"]["utD"].value = d;
+    document.forms["origin-destination"]["utH"].value = h;
+    document.forms["origin-destination"]["utM"].value = m;
+
+    document.forms["origin-destination"]["origin"].value = originName;
+    document.forms["origin-destination"]["destination"].value = destinationName;
+
+    console.log("initialized from history");
+
+    return true;
+}
+
+function initialize() {
+
+    getPlanetsXML(() => {
+
+    initializeFromQueryString();
+
+    window.addEventListener("displayedTimeChange", displayedTimeChange);
+
+    let originOpt = document.getElementById("originSet")
+
+    originOpt.addEventListener("click", originChange)
+    originOpt.addEventListener("change", (e) => svgTxOrbit.eventHandler(e));
+    originOpt.value = originName;
+
+    let destOpt = document.getElementById("destinationSet")
+    destOpt.addEventListener("click", destinationChange)
+    destOpt.addEventListener("change", (e) => svgTxOrbit.eventHandler(e));
+    destOpt.value = destinationName;
+
+
+    initializeScreen();
+    initializeSolarSystemSVG();
+    setSolarSystemSVG();
+    initializeTransferOrbit();
+    initializeTransferSVG();
+
+    currentTimeChange();
+
+    //displayedTime=txOrbit.solveTForRdv(currentTime);
+    //window.dispatchEvent(displayedTimeChangeEvent);
+
+    zoomTxOrbit();
+
+    console.log("intialized");
+
+    });
+}
